f976de966f2bf32ab0146de65a35b3d3284dbca3
Ekaterina Chernyshova <newfatto@gmail.com>
2026-04-19 17:39:24 +0300

    Добавлено обновление timezone в профиль пользователя

--- users/validators.py
+++ users/validators.py
@@ -1,14 +1,20 @@
+import re
 from zoneinfo import ZoneInfo, ZoneInfoNotFoundError
 
 from django.core.exceptions import ValidationError
 
 
+UTC_OFFSET_PATTERN = re.compile(r"^UTC[+-](0\d|1[0-4]):00$")
+
+
 def validate_time_zone(value: str) -> None:
-    """Проверяет, что строка является корректным IANA-часовым поясом."""
-    if not value:
-        raise ValidationError("Укажите часовой пояс.")
+    """Проверяет корректность часового пояса."""
+    if UTC_OFFSET_PATTERN.fullmatch(value):
+        return
 
     try:
         ZoneInfo(value)
     except ZoneInfoNotFoundError as exc:
-        raise ValidationError("Укажите корректный часовой пояс, например Europe/Moscow.") from exc
+        raise ValidationError(
+            "Укажите корректный часовой пояс."
+        ) from exc
